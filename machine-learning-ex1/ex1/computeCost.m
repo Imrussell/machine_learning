607ad33149cef3c5a84d2621336da7e3326a36d9
Ian <imrussell2000@yahoo.co.uk>
2018-06-23 16:24:50 +0100

    first commit

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -8,6 +8,9 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
+x = 0;
+h = 0;
+q = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
@@ -15,12 +18,21 @@ J = 0;
 
 %fprintf("m: %d \n",m)
 
-for q = 1:m
-     x = X(q,2);
-     h = (theta' * x );
-     % fprintf("q: %d, h: %d, x= %d \n",q, h, x);
-     J = J + 1/(2*m) * (h*x - y(q))^2;
-end
+%keyboard
+
+
+
+predictions = X*theta;              % predictions of hypothesis on examples
+sqrErrors   = (predictions - y).^2; % squared errors
+
+J = 1/(2*m) * sum(sqrErrors);
+
+%for q = 1:m
+%     x = X(q,2);
+%     h = (theta' * x );
+ %    fprintf("J: %d    q: %d, h: %d, x= %d \n",J, q, h, x);
+%     J = J + 1/(2*m) * (h*x - y(q)).^2;
+%end
 
      %J=32.07;
 
